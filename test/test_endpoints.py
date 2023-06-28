26670733ad9c5c226e0d6ddefe6af544e698d074
unknown <tomiwaaribisala@gmail.com>
2023-06-28 12:31:10 +0100

    uber-python-app

--- test/test_endpoints.py
+++ test/test_endpoints.py
@@ -1,7 +1,14 @@
 import unittest
 
 from betamax import Betamax
-from app import app
+
+import os 
+import sys
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+from app import app 
+
 
 with Betamax.configure() as config:
     config.cassette_library_dir = 'test/fixtures'
